40397a8fa842e07eb15d8fbe3abd1f1b3de18743
Marko Lauer <marko@mlkonsultatsioonid.ee>
2023-12-05 20:30:23 +0200

    week 12

---
+++ db.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { Pool } = require('pg');
+
+const itemsPool = new Pool ({
+    connectionString: process.env.DBConnectionString, //lisasin ?ssl=true
+    ssl: {
+        rejectUnauthorized: false
+}
+});
+
+// const itemsPool = new Pool({
+//     user: 'postgres',
+//     password: '490124',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'postgres'
+// });
+
+module.exports = itemsPool;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const recipeRouter = require('./routes/recipes.routes');
+const ingredientRouter = require('./routes/ingredients.routes');
+const fullRecipesRouter = require('./routes/fullRecipes.routes');
+const randomRouter = require('./routes/randomRecipes.routes');
+const app = express();
+
+app.use(express.json());
+
+app.use('/recipes', recipeRouter);
+app.use('/ingredients', ingredientRouter);
+app.use('/fullrecipes', fullRecipesRouter);
+app.use('/random', randomRouter);
+
+app.listen(3000, () =>{
+    
+});
